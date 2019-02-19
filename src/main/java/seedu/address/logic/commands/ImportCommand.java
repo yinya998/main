07fd183a3e287ce53fa8ecbc52e5c3613f42e56b
daDangminh <minhdang619@yahoo.com.vn>
2019-02-19 14:26:38 +0800

    checkstyle fix

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -10,21 +9,20 @@ import seedu.address.model.Model;
  */
 public class ImportCommand extends Command {
 
-	public static final String COMMAND_WORD = "import";
-	public static final String MESSAGE_NOT_IMPLEMENTED = "Import command not implemented yet";
+    public static final String COMMAND_WORD = "import";
+    public static final String MESSAGE_NOT_IMPLEMENTED = "Import command not implemented yet";
 
-	public static final String MESSAGE_USAGE = COMMAND_WORD
-			+ ": Imports contacts using a path to an .xml file.\n"
-			+ "Parameters: PATH\n"
-			+ "Example: " + COMMAND_WORD + " data/contacts.xml";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Imports contacts using a path to an .xml file.\n"
+            + "Parameters: PATH\n"
+            + "Example: " + COMMAND_WORD + " data/contacts.xml";
 
 
-	public ImportCommand(String importPath) {
+    public ImportCommand() {
+    }
 
-	}
-
-	@Override
-	public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-		throw new CommandException(MESSAGE_NOT_IMPLEMENTED);
-	}
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED);
+    }
 }
--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -10,17 +8,17 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class ImportCommandParser implements Parser<ImportCommand> {
 
-	public static final String MESSAGE_PATH_PARSER_NOT_IMPLEMENTED = "Path parser not implemented yet";
+    public static final String MESSAGE_PATH_PARSER_NOT_IMPLEMENTED = "Path parser not implemented yet";
 
-	/**
-	 * Parses the given {@code String} of arguments in the context of the DeleteCommand
-	 * and returns an DeleteCommand object for execution.
-	 * @throws ParseException if the user input does not conform the expected format
-	 */
-	public ImportCommand parse(String args) throws ParseException {
-		throw new ParseException(MESSAGE_PATH_PARSER_NOT_IMPLEMENTED);  //TODO: implement path parser in ParserUtil
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns an DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ImportCommand parse(String args) throws ParseException {
+        throw new ParseException(MESSAGE_PATH_PARSER_NOT_IMPLEMENTED);  //TODO: implement path parser in ParserUtil
 
-		// TODO: return an ImportCommand object here
-	}
+        // TODO: return an ImportCommand object here
+    }
 
 }
