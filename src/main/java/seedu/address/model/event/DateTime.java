37622745e41454bd4c279e1d842f6b6a70a30c18
Wulin Jiang <e0134110@u.nus.edu>
2019-02-28 22:36:19 +0800

    build event list model

--- src/main/java/seedu/address/model/event/DateTime.java
+++ src/main/java/seedu/address/model/event/DateTime.java
@@ -1,8 +1,52 @@
 package seedu.address.model.event;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents an event's date and time in the event list
- * Guarantees: immutable;
+ * Guarantees: immutable;is valid as declared in {@link #isValidDateTime(String)}
  */
 public class DateTime {
+
+
+    public static final String MESSAGE_CONSTRAINTS = "Datetime should be of the format of yyyy-mm-dd hh:mm:ss";
+    public static final String VALIDATION_REGEX = "\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}";
+    public final String value;
+
+    /**
+     * Constructs a {@code DateTime}.
+     *
+     * @param dateTime A valid dateTime.
+     */
+    public DateTime(String dateTime) {
+        requireNonNull(dateTime);
+        checkArgument(isValidDateTime(dateTime), MESSAGE_CONSTRAINTS);
+        value = dateTime;
+    }
+
+    /**
+     * Returns true if a given string is a valid date time.
+     */
+    public static boolean isValidDateTime(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof seedu.address.model.event.DateTime // instanceof handles nulls
+                && value.equals(((seedu.address.model.event.DateTime) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
 }
