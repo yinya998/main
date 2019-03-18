6019aeb6eeb1376484b0d38cca27258aa2215697
JillWangJill <jerryname652@gmail.com>
2019-03-18 12:31:30 +0800

    Reminder.java; reminder class, travis bug fix

--- src/main/java/seedu/address/model/reminder/Reminder.java
+++ src/main/java/seedu/address/model/reminder/Reminder.java
@@ -3,6 +3,7 @@ package seedu.address.model.reminder;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Objects;
+
 import seedu.address.model.event.Event;
 
 /**
