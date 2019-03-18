608de029d842e88d50299cd0dcc6400e3db9b061
JillWangJill <jerryname652@gmail.com>
2019-03-18 12:25:26 +0800

    reminder.java; a reminder class, travis bug fix

--- src/main/java/seedu/address/model/reminder/Reminder.java
+++ src/main/java/seedu/address/model/reminder/Reminder.java
@@ -1,56 +1,72 @@
 package seedu.address.model.reminder;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.util.Objects;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import seedu.address.model.event.Event;
 
+/**
+ *  Represents a Reminder in the address book.
+ *  Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Reminder {
     private Event event;
     private String message;
     private String name;
 
-    public Reminder(Event event, String message){
-        requireAllNonNull(event,message);
-        this.event=event;
-        this.message=message;
+    /**
+     * Every filed must be present and not NULL
+     */
+    public Reminder(Event event, String message) {
+        requireAllNonNull(event, message);
+        this.event = event;
+        this.message = message;
     }
 
-    public Reminder(String name, String message){
-        requireAllNonNull(name,message);
-        this. name =name;
-        this.message=message;
+    public Reminder(String name, String message) {
+        requireAllNonNull(name, message);
+        this.name = name;
+        this.message = message;
     }
 
-    public Reminder (Reminder source){
-        this(source.getEvent(),source.getMessage());
+    public Reminder (Reminder source) {
+        this(source.getEvent(), source.getMessage());
     }
 
-    public void setMessage(String message){
+    /**
+     * Set the message of reminder, reminder message must be present and not NULL
+     * @param message
+     */
+    public void setMessage(String message) {
         requireAllNonNull(message);
         this.message=message;
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return message;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
     //TODO: implement getEvent, which needs a get() in event class, event file
-    public Event getEvent(){
+    public Event getEvent() {
         return event;
     }
+
+    /**
+     * Returns true if both reminders have same data filed.
+     * This defines a stronger notion of equality between two reminders.
+     * @param other
+     * @return
+     */
     @Override
-    public boolean equals(Object other){
-        if(other==this){
+    public boolean equals(Object other) {
+        if (other == this) {
             return true;
         }
 
-        if (other instanceof Reminder && this.equals((Reminder)other) ){
+        if (other instanceof Reminder && this.equals((Reminder) other)) {
             return true;
         }
 
@@ -58,7 +74,7 @@ public class Reminder {
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return Objects.hash(event, message);
     }
 
