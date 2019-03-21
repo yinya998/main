5e49fee1837cbe8eb59201c2bb322ffc1ab1633b
Wulin Jiang <e0134110@u.nus.edu>
2019-03-21 12:06:03 +0800

    travis bug fix

--- src/main/java/seedu/address/logic/parser/ConnectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ConnectCommandParser.java
@@ -4,7 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_INDEX;
 
-import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
