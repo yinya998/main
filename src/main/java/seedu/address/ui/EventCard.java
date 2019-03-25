24655fdf1146874c82a51ade814ad74651ae39ee
yonggqiii <yongqifoo@gmail.com>
2019-03-25 15:25:57 +0800

    more checkstyle

--- src/main/java/seedu/address/ui/EventCard.java
+++ src/main/java/seedu/address/ui/EventCard.java
@@ -7,6 +7,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.event.Event;
 
+/**
+ * A UI component that displays information of a {@code Event}.
+ */
 public class EventCard extends UiPart<Region> {
 
     private static final String FXML = "EventListCard.fxml";
