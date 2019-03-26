007fcbca0a48db1ce33e44d6053fc51422ecc4d4
yinya998 <yinya998@gmail.com>
2019-03-27 00:33:00 +0800

    f

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -35,8 +35,8 @@ public class CommandTestUtil {
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_PHOTO_AMY = "d:dir/test.jpg";
-    public static final String VALID_PHOTO_BOB = "d:dir/test.jpg";
+    public static final String VALID_PHOTO_AMY = "docs/images/test4.jpg";
+    public static final String VALID_PHOTO_BOB = "docs/images/test4.jpg";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
