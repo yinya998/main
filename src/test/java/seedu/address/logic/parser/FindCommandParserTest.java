9df2458ea3ab97dc6e4551b5539a769acaab8cbc
yinya998 <Yinya998@gmail.com>
2019-03-30 15:50:08 +0800

    travis

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,11 +1,13 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.FindECommand.MESSAGE_NO_PARAMETER;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindECommand;
 
 public class FindCommandParserTest {
 
@@ -13,7 +15,7 @@ public class FindCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_NO_PARAMETER + "\n" + FindECommand.MESSAGE_USAGE));
     }
 
     /*@Test
