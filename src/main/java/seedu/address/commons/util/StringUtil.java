9898ff7e2eea3265b7678c5c950d60bea9aa67a1
yinya998 <Yinya998@gmail.com>
2019-04-01 11:50:50 +0800

    resolve conflict

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -1,74 +1,3 @@
-<<<<<<< Updated upstream
-package seedu.address.commons.util;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Arrays;
-
-/**
- * Helper functions for handling strings.
- */
-public class StringUtil {
-
-    /**
-     * Returns true if the {@code sentence} contains the {@code word}.
-     *   Ignores case, but a full word match is required.
-     *   <br>examples:<pre>
-     *       containsWordIgnoreCase("ABc def", "abc") == true
-     *       containsWordIgnoreCase("ABc def", "DEF") == true
-     *       containsWordIgnoreCase("ABc def", "AB") == false //not a full word match
-     *       </pre>
-     * @param sentence cannot be null
-     * @param word cannot be null, cannot be empty, must be a single word
-     */
-    public static boolean containsWordIgnoreCase(String sentence, String word) {
-        requireNonNull(sentence);
-        requireNonNull(word);
-
-        String preppedWord = word.trim();
-        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
-
-        String preppedSentence = sentence;
-        String[] wordsInPreppedSentence = preppedSentence.split("\\s+");
-
-        return Arrays.stream(wordsInPreppedSentence)
-            .anyMatch(preppedWord::equalsIgnoreCase);
-    }
-
-    /**
-     * Returns a detailed message of the t, including the stack trace.
-     */
-    public static String getDetails(Throwable t) {
-        requireNonNull(t);
-        StringWriter sw = new StringWriter();
-        t.printStackTrace(new PrintWriter(sw));
-        return t.getMessage() + "\n" + sw.toString();
-    }
-
-    /**
-     * Returns true if {@code s} represents a non-zero unsigned integer
-     * e.g. 1, 2, 3, ..., {@code Integer.MAX_VALUE} <br>
-     * Will return false for any other non-null string input
-     * e.g. empty string, "-1", "0", "+1", and " 2 " (untrimmed), "3 0" (contains whitespace), "1 a" (contains letters)
-     * @throws NullPointerException if {@code s} is null.
-     */
-    public static boolean isNonZeroUnsignedInteger(String s) {
-        requireNonNull(s);
-
-        try {
-            int value = Integer.parseInt(s);
-            return value > 0 && !s.startsWith("+"); // "+1" is successfully parsed by Integer#parseInt(String)
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-    }
-
-}
-=======
 package seedu.address.commons.util;
 
 import static java.util.Objects.requireNonNull;
@@ -127,7 +56,7 @@ public class StringUtil {
 
     public static double sim(String str1, String str2) {
         try {
-            double ld = (double)ld(str1, str2);
+            double ld = (double) findLevenshteinDistance(str1, str2);
             return (1-ld/(double)Math.max(str1.length(), str2.length()));
         } catch (Exception e) {
             return 0.1;
@@ -168,7 +97,7 @@ public class StringUtil {
         }
         return min;
     }
-    public static int ld(String str1, String str2) {
+    public static int findLevenshteinDistance(String str1, String str2) {
         int d[][];
         int n = str1.length();
         int m = str2.length();
@@ -235,4 +164,3 @@ public class StringUtil {
     }
 
 }
->>>>>>> Stashed changes
