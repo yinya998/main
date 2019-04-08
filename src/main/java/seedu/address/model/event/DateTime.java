5fd0c135dfce329b8ddd17f4870d4759b2359225
Wulin Jiang <e0134110@u.nus.edu>
2019-04-08 20:36:47 +0800

    handle datetime invalid cases

--- src/main/java/seedu/address/model/event/DateTime.java
+++ src/main/java/seedu/address/model/event/DateTime.java
@@ -1,5 +1,9 @@
 package seedu.address.model.event;
 
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -10,7 +14,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class DateTime {
 
 
-    public static final String MESSAGE_CONSTRAINTS = "Datetime should be of the format of yyyy-mm-dd hh:mm:ss";
+    public static final String MESSAGE_CONSTRAINTS = "Datetime should be of the format of yyyy-mm-dd hh:mm:ss, " +
+            "please check whether your date and time are valid";
     public static final String VALIDATION_REGEX = "\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}";
     public final String value;
 
@@ -29,7 +34,19 @@ public class DateTime {
      * Returns true if a given string is a valid date time.
      */
     public static boolean isValidDateTime(String test) {
-        return test.matches(VALIDATION_REGEX);
+        boolean isValidDateTime = false;
+        if (test.matches(VALIDATION_REGEX)) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            format.setLenient(false);
+            try {
+                format.parse(test);
+                isValidDateTime = true;
+            }
+            catch (ParseException e) {
+            }
+
+        }
+        return isValidDateTime;
     }
 
     @Override
@@ -42,6 +59,28 @@ public class DateTime {
         return other == this // short circuit if same object
                 || (other instanceof seedu.address.model.event.DateTime // instanceof handles nulls
                 && value.equals(((seedu.address.model.event.DateTime) other).value)); // state check
+    }
+
+    public boolean isBefore(Object other) {
+        boolean isStartBeforeEnd = false;
+        if(other instanceof seedu.address.model.event.DateTime){
+            DateTime endDateTime = (seedu.address.model.event.DateTime) other;
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            format.setLenient(false);
+            try
+            {
+                Date start = format.parse(this.value);
+                Date end = format.parse(endDateTime.value);
+                if(start.before(end) || start.equals(end)){
+                    isStartBeforeEnd = true;
+                }
+            }
+            /* Date format is invalid */
+            catch (ParseException e)
+            {
+            }
+        }
+        return isStartBeforeEnd;
     }
 
     @Override
