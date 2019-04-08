ba32dd475fb9d3f3df7665acc30aecbd69c523d4
JillWangJill <jerryname652@gmail.com>
2019-04-08 22:11:33 +0800

    travis

--- src/test/java/seedu/address/model/reminder/IntervalTest.java
+++ src/test/java/seedu/address/model/reminder/IntervalTest.java
@@ -1,11 +1,10 @@
 package seedu.address.model.reminder;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 
 public class IntervalTest {
     private static final Interval interval = new Interval("2", "min");
--- src/test/java/seedu/address/model/reminder/UnitTest.java
+++ src/test/java/seedu/address/model/reminder/UnitTest.java
@@ -1,11 +1,10 @@
 package seedu.address.model.reminder;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 
 public class UnitTest {
     private static final Unit unit = new Unit("min");
