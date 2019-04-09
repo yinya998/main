09dd9103c0e7b156d40d02ccb1a9a3b527c2cbc7
Wulin Jiang <e0134110@u.nus.edu>
2019-04-09 13:28:18 +0800

    bug fix

--- src/main/java/seedu/address/logic/commands/AddECommand.java
+++ src/main/java/seedu/address/logic/commands/AddECommand.java
@@ -12,7 +12,6 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.event.Event;
-import seedu.address.model.reminder.Reminder;
 import seedu.address.ui.WindowViewState;
 
 
