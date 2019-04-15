1cd27792d0a93cf801019659aed7aa25dead476f
yonggqiii <yongqifoo@gmail.com>
2019-04-15 19:33:23 +0800

    Switch Command at 100% coverage

---
+++ src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.commands;
+
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertEventCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+public class SwitchCommandTest {
+
+    @Test
+    public void switchTest() {
+        SwitchCommand switcher = new SwitchCommand();
+        assertCommandSuccess(switcher,
+                new ModelManager(getTypicalAddressBook(), new UserPrefs()),
+                new CommandHistory(),
+                "Switched to events view.",
+                new ModelManager(getTypicalAddressBook(), new UserPrefs()));
+        assertEventCommandSuccess(switcher,
+                new ModelManager(getTypicalAddressBook(), new UserPrefs()),
+                new CommandHistory(),
+                "Switched to persons view.",
+                new ModelManager(getTypicalAddressBook(), new UserPrefs()));
+    }
+}
